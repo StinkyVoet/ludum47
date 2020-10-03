23218655b2269af02783dd9cae0dc04f0b32abcd
Calvin <stinkyvoet@gmail.com>
2020-10-03 17:31:54 +0200

    [Feature] Added Basic Playermovement
    also has walljumps

---
+++ Ludum47/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    [Header("Movement Settings")]
+    public float speed = 5;
+    public float jumpForce = 2f;
+
+    [Header("Groundcheck Settings")]
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+    bool wallCollision;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundDistance, groundMask);
+
+        float x = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        } 
+        else if(Input.GetButtonDown("Jump") && wallCollision)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        wallCollision = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        wallCollision = false;
+    }
+}
